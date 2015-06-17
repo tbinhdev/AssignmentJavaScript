c56a8903ba1d937dde1ac6c791000801ca9aa191
Restaurant <binhtruong2411@gmail.com>
2015-06-17 14:36:52 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+/**
+ * Created by Carot on 6/4/2015.
+ */
+$(document).ready(function () {
+   $('section#screenshots a').on('click', function () {
+        $('div#modal img').attr('src',$(this).attr('data-image-url'));
+   });
+
+   var nav = $('.navbar-fixed-top');
+
+   var distance = $('.navbar-fixed-top').offset();
+
+   	if(distance.top >= 300){
+   		nav.addClass('effect');
+   	}
+
+
+   $(window).scroll(function(){
+   		var scroll = $(window).scrollTop();
+   		if (scroll >= 300) {
+   			nav.addClass('effect');
+   		}else{
+   			nav.removeClass('effect');
+   		}
+   });
+	$('#about .blue-circle').waypoint(function(){
+		$('#about .blue-circle').addClass('animated fadeInUp')
+	}, {
+		offset: '50%'
+	});
+	$('#features .blue-circle').waypoint(function(){
+		$(this.element).addClass('animated fadeInUp');
+	}, {
+		offset: '50%'
+	});
+	$('.features-image img').waypoint(function(){
+		$('.features-image img').addClass('animated rubberBand')
+	}, {
+		offset: '50%'
+	});
+
+	$('#screenshots .col-sm-4').waypoint(function(){
+		$(this.element).addClass('animated zoomIn');
+		$(this.element).css({'opacity' : 1});
+	}, {
+		offset: '50%'
+	});
+	$('#download div.phone img').waypoint(function(){
+		$(this.element).addClass('animated fadeInRight');
+
+	}, {
+		offset: '50%'
+	});
+
+	$('#download .platforms > div').waypoint(function(){
+		$(this.element).addClass('animated fadeInUp');
+
+	}, {
+		offset: '50%'
+	});
+
+	$('#form'.bo)
+});
+
+smoothScroll.init({
+	speed: 700,
+	easing: 'easeInOutQuad',
+	updateURL: false,
+	offset: 0
+
+});
